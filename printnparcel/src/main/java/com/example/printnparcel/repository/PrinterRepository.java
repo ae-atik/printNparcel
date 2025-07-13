47c05fd3f033058217f7a751303722381d0d54ec
ae-atik <atik31172@gmail.com>
2025-07-14 01:17:12 +0600

    back_removed

--- printnparcel/src/main/java/com/example/printnparcel/repository/PrinterRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.printnparcel.repository;
-
-import com.example.printnparcel.model.Printer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrinterRepository extends JpaRepository<Printer, Long> { }
